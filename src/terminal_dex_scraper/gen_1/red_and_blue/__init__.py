264d8ca3ab1cb00040c85bdad5f07a2d61ecddb0
VictorF13 <victorabreuf13@gmail.com>
2025-09-22 20:05:59 -0300

    feat: add initial files for Pokémon Red and Blue data scraping

---
+++ src/terminal_dex_scraper/gen_1/red_and_blue/__init__.py
@@ -0,0 +1,1 @@
+"""Package to scrape the data for a given Pokémon in Red and Blue."""
---
+++ src/terminal_dex_scraper/gen_1/red_and_blue/pokemon_data.py
@@ -0,0 +1,37 @@
+"""Module to scrape the data for a given Pokémon in Red and Blue.
+
+This module aims to get the relevant data for a given Pokémon in Red and Blue. The
+source of the data is the Pokémon Red and Blue disassembly and the Pokémon Asset
+Archive.
+
+This is the data this module scrapes:
+
+- Basic Pokédex Information:
+    - Pokédex Number;
+    - Pokémon Name;
+    - Pokédex Entry;
+    - Species;
+    - Height;
+    - Weight;
+    - Cry.
+- Types;
+- Pokémon Location:
+    - Location and method of capture in Pokémon Red and Blue;
+    - In-game events;
+    - Event Distributions.
+- Base Stats;
+- Catch Rate;
+- Leveling rate;
+- Base experience yield;
+- Learnset:
+    - Level-up;
+    - Machine.
+- Evolution data;
+- Sprites:
+    - Front and back monochrome;
+    - Front and back colored;
+    - Party Sprite monochrome;
+    - Party Sprite colored.
+- Artwork / Gallery:
+    - Ken Sugimori's official artwork for Red and Blue.
+"""
