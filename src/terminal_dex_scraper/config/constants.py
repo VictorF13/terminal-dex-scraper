51ff5055fec8f3736e19d9515f246bba15de5269
VictorF13 <victorabreuf13@gmail.com>
2025-09-22 20:32:51 -0300

    feat: introduce constants for Pokémon Red and Blue disassembly and update settings to use them

---
+++ src/terminal_dex_scraper/config/constants.py
@@ -0,0 +1,11 @@
+"""Module containing constants for the Terminal Dex Scraper."""
+
+from pathlib import Path
+
+# Disassembly repositories URLs
+POKEMON_RED_AND_BLUE_DISASSEMBLY_REPO_URL = "git@github.com:pret/pokered.git"
+
+# Disassembly repositories target paths
+POKEMON_RED_AND_BLUE_DISASSEMBLY_PATH = Path(
+    "src/terminal_dex_scraper/gen_1/red_and_blue/disassembly"
+)
--- src/terminal_dex_scraper/config/settings.py
+++ src/terminal_dex_scraper/config/settings.py
@@ -2,8 +2,14 @@
 
 from pydantic_settings import BaseSettings
 
+from terminal_dex_scraper.config.constants import (
+    POKEMON_RED_AND_BLUE_DISASSEMBLY_REPO_URL,
+)
+
 
 class Settings(BaseSettings):
     """Settings for the Terminal Dex Scraper."""
 
-    pokered_disassembly_repo: str = "git@github.com:pret/pokered.git"
+    pokemon_red_and_blue_disassembly_repo: str = (
+        POKEMON_RED_AND_BLUE_DISASSEMBLY_REPO_URL
+    )
