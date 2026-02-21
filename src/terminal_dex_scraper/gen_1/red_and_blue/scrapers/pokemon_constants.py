334eda24f6ee34a7215188b0efebe43b2c6f7f25
VictorF13 <victorabreuf13@gmail.com>
2026-02-21 17:47:00 -0300

    fix: update serialize_records method to handle None values in Pokémon constants

--- src/terminal_dex_scraper/gen_1/red_and_blue/scrapers/pokemon_constants.py
+++ src/terminal_dex_scraper/gen_1/red_and_blue/scrapers/pokemon_constants.py
@@ -111,12 +111,12 @@ class PokemonConstants:
         constant = self.aliases.get(pokemon_constant, pokemon_constant)
         return self.constants.index(constant)
 
-    def serialize_records(self) -> list[dict[str, int | str]]:
+    def serialize_records(self) -> list[dict[str, int | str | None]]:
         """Build JSON-ready records for Pokémon constants.
 
         Returns:
-            list[dict[str, int | str]]: A list of records where each record contains
-                the constant index and name.
+            list[dict[str, int | str | None]]: A list of records where each record
+                contains the constant index and name.
 
         """
         return [
@@ -125,7 +125,6 @@ class PokemonConstants:
                 "pokemon_constant_name": constant_name,
             }
             for constant_index, constant_name in enumerate(self.constants)
-            if constant_name is not None
         ]
 
     def serialize_alias_records(self) -> list[dict[str, int | str]]:
